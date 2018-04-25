9e6428cd5578c02e95eaa32f16ea56c433223d3c
JHades <jhades.dev@gmail.com>
2018-04-25 11:55:21 +0200

    View Child examples

--- src/app/color-sample/color-sample.component.ts
+++ src/app/color-sample/color-sample.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, Input, OnInit} from '@angular/core';
 
 @Component({
   selector: 'color-sample',
@@ -6,6 +6,8 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./color-sample.component.css']
 })
 export class ColorSampleComponent implements OnInit {
+
+  @Input() color;
 
   constructor() { }
 
